c9c609ff90d5caeec51e282bd46dbf16eb8ffc2e
Wolfgang Beyer <wolfi@chromium.org>
2026-01-29 14:42:26 +0000

    Update visible UI

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,13 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
+    <h1>DevTools MCP & Runtime Data</h1>
+    <p class="text">
+      This demo page listens to the 'devtoolsdiscovery' event, and calls the event's 'respondWith()' method to provide a list of tools.
     </p>
   </div>
 `
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
 
 interface ToolDefinition {
   name: string;
